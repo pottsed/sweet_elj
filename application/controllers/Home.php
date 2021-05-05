a4be8bf92c2bc68acaf30c88b093dad88ccc7fe5
Edward Potts <46508197+pottsed@users.noreply.github.com>
2021-05-05 10:45:57 +0700

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,7 +20,7 @@ class Home extends CI_Controller
         $this->load->library('session');
 
         // Userid 
-        $this->session->set_userdata(array("id_pelanggan" => 3));
+        // $this->session->set_userdata(array("id_pelanggan" => 3));
     }
 
 
@@ -54,69 +54,69 @@ class Home extends CI_Controller
     }
     public function comment($id_barang)
     {
-         $data = array(
-            'id_barang' => $id_barang,
-            'user'      => $this->input->post('user'),
-            'rating'    => $this->input->post('rating'),
-            'comment'   => $this->input->post('comment')
-        );
-        $this->m_rating->add($data);
-        $this->session->set_flashdata('pesan', 'Data Berhasil Ditambahkan !');
-        redirect('home/detail_brg/'.$id_barang, 'refresh');
-    }
+       $data = array(
+        'id_barang' => $id_barang,
+        'user'      => $this->input->post('user'),
+        'rating'    => $this->input->post('rating'),
+        'comment'   => $this->input->post('comment')
+    );
+       $this->m_rating->add($data);
+       $this->session->set_flashdata('pesan', 'Data Berhasil Ditambahkan !');
+       redirect('home/detail_brg/'.$id_barang, 'refresh');
+   }
 
-    public function kategori($id_kategori)
-    {
-        $kategori = $this->m_home->kategori($id_kategori);
-        $data = array(
-            'tittle' => 'Kategori Barang :' . $kategori->nama_kategori,
-            'barang' => $this->m_home->get_all_data_brg($id_kategori),
-            'isi'   => 'v_kategori_brg'
+   public function kategori($id_kategori)
+   {
+    $kategori = $this->m_home->kategori($id_kategori);
+    $data = array(
+        'tittle' => 'Kategori Barang :' . $kategori->nama_kategori,
+        'barang' => $this->m_home->get_all_data_brg($id_kategori),
+        'isi'   => 'v_kategori_brg'
 
-        );
-        $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
-    }
+    );
+    $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
+}
 
-    public function bahan($id_bahan)
-    {
-        $bahan = $this->m_home->bahan($id_bahan);
-        $data = array(
-            'tittle' => 'Bahan Barang : ' . $bahan->nama_bahan,
-            'bahan' => $this->m_home->get_all_data_bahan($id_bahan),
-            'isi'   => 'v_bahan'
+public function bahan($id_bahan)
+{
+    $bahan = $this->m_home->bahan($id_bahan);
+    $data = array(
+        'tittle' => 'Bahan Barang : ' . $bahan->nama_bahan,
+        'bahan' => $this->m_home->get_all_data_bahan($id_bahan),
+        'isi'   => 'v_bahan'
 
-        );
-        $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
-    }
+    );
+    $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
+}
 
-    public function detail_brg($id_barang)
-    {
-        $data = array(
-            'tittle' => 'Detail Barang',
-            'gambar' => $this->m_home->gambar_brg($id_barang),
-            'barang' => $this->m_home->detail_brg($id_barang),
-            'rating' => $this->m_rating->viewRating($id_barang),
-            'isi'   => 'v_detail'
+public function detail_brg($id_barang)
+{
+    $data = array(
+        'tittle' => 'Detail Barang',
+        'gambar' => $this->m_home->gambar_brg($id_barang),
+        'barang' => $this->m_home->detail_brg($id_barang),
+        'rating' => $this->m_rating->viewRating($id_barang),
+        'isi'   => 'v_detail'
 
-        );
-        $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
-    }
+    );
+    $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
+}
 
     // Update rating
-    public function updateRating()
-    {
+public function updateRating()
+{
 
         // userid
-        $userid = $this->session->userdata('id_pelanggan');
+    $userid = $this->session->userdata('id_pelanggan');
 
         // POST values
-        $postid = $this->input->post('id_barang');
-        $rating = $this->input->post('rating');
+    $postid = $this->input->post('id_barang');
+    $rating = $this->input->post('rating');
 
         // Update user rating and get Average rating of a post
-        $averageRating = $this->m_rating->userRating($userid, $postid, $rating);
+    $averageRating = $this->m_rating->userRating($userid, $postid, $rating);
 
-        echo $averageRating;
-        exit;
-    }
+    echo $averageRating;
+    exit;
+}
 }
